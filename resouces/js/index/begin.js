8eb4249f4a4a7e3ceebc3907480dbaa9d2a53ea1
Alex-McAvoy <931772462@qq.com>
2020-05-18 18:02:04 +0800

    Update begin.js

--- resouces/js/index/begin.js
+++ resouces/js/index/begin.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+$(document).ready(function() {
 	//轮播图设置
 	$('.carousel').carousel({
 		interval: 3000
@@ -34,4 +34,4 @@ window.onload = function() {
 			return false;
 		};
 	})(jQuery);
-}
+});
