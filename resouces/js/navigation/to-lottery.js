7f8f9224b779d1c26c123ab58f7b1ed620cb64c6
Alex-McAvoy <931772462@qq.com>
2020-05-23 01:47:48 +0800

    Update to-lottery.js

--- resouces/js/navigation/to-lottery.js
+++ resouces/js/navigation/to-lottery.js
@@ -1,3 +1,14 @@
+$(document).ready(function() {
+	//读取开奖信息json
+	$.ajax({
+		url: '../resouces/json/lottery.json',
+		async: false,
+		success: function(data) {
+			num = data.num;
+		}
+	});
+});
+
 //随机url参数生成
 function updateNum(){
 	var table=["1","2","3","4","5","6","7","8","9","10","A","B","C","D","E","F"];
