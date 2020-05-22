93a56b5fdc23eb858000d0a1d1dbb95a47cc5115
Alex-McAvoy <931772462@qq.com>
2020-05-23 01:48:54 +0800

    Update begin.js

--- resouces/js/index/begin.js
+++ resouces/js/index/begin.js
@@ -3,14 +3,6 @@ $(document).ready(function() {
 	$('.carousel').carousel({
 		interval: 3000
 	});
-	//读取开奖信息json
-	$.ajax({
-		url: '../resouces/json/lottery.json',
-		async: false,
-		success: function(data) {
-			num = data.num;
-		}
-	});
 	//读取中奖汇总json
 	$.ajax({
 		url: '../resouces/json/prizes_table.json',
