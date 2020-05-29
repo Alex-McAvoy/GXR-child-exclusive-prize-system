602baaf1cf278dcb024aaa95de4059784430841f
Alex-McAvoy <931772462@qq.com>
2020-05-30 01:37:30 +0800

    Update to-lottery.js

--- resouces/js/navigation/to-lottery.js
+++ resouces/js/navigation/to-lottery.js
@@ -1,24 +1,18 @@
-$(document).ready(function() {
-	//读取开奖信息json
-	$.ajax({
-		url: '../resouces/json/lottery.json',
-		async: false,
-		success: function(data) {
-			num = data.num;
-		}
-	});
-});
-
 //随机url参数生成
-function updateNum(){
+function updateNum(num){
 	var table=["1","2","3","4","5","6","7","8","9","10","A","B","C","D","E","F"];
 	for(var i=1;i<=5;i++){
 		var rand = Math.floor((Math.random()*16)); //获取随机数
 		num=num+table[rand];
 	}
+	return num;
 }
 
 function toLottery(){
-	updateNum(); //更新num参数
+	var num=sessionStorage.getItem("num");
+	if(num==""||num=="error"||num==null) //session信息不存在
+		num=updateNum(0);
+	else
+		num=updateNum(num); //更新num参数
 	window.location.href="./lottery.html?num="+num;
 }
